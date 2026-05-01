80bb28c52cf3cb473f4bcd30c7b1918bf9bc792a
ccccxn <its3rbb@gmail.com>
2026-05-01 14:38:35 +0300

    Simplify permission check for command usage
    Removed role check for sending permission denied message.

--- src/config/bot.js
+++ src/config/bot.js
@@ -543,12 +543,6 @@ export function getRandomColor() {
 }
 
 export default botConfig;
-
-if(message.member.roles.cache.has('1499424250174640139')){
-  message.channel.send('kaddnoroles')
-
-  
-} else {
 message.channel.send('⛔ Permission Denied
 You are not allowed to use this command.
 ')
